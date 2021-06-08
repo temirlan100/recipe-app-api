77730211cc105a2d215f398dd77e100c5c03fd5a
temirlan100 <temirlan1990@gmail.com>
2021-06-08 17:20:04 +0300

    Fix: createsuperuse valid

--- app/core/models.py
+++ app/core/models.py
@@ -18,7 +18,7 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password):
         """Create and save new superuser"""
-        user = self.model(email=email, password=password)
+        user = self.create_user(email=email, password=password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
