9e95177528285326eae1bc0fb5e7ee736439145d
temirlan100 <temirlan1990@gmail.com>
2021-06-09 10:21:57 +0300

    Tests: create user

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,60 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+CREATE_USER_URL = reverse("user:create")
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserApiTests(TestCase):
+    """Test the users API (public)"""
+
+    def setUp(self):
+        self.client = APIClient
+
+    def test_create_valid_user_success(self):
+        """Test creating with valid user payload successful"""
+        payload = {
+            "email": "test@test.com",
+            "password": "some_pass",
+            "name": "test_name"
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(user.check_password(payload.get("password")))
+        self.assertNotIn("password", res.data)
+
+    def test_user_exists(self):
+        """Test creating user that already exists fail"""
+        payload = {
+            "email": "test@test.com",
+            "password": "some_pass"
+        }
+        create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short(self):
+        """Test password length more than 5 characters"""
+        payload = {
+            "email": "test@test.com",
+            "password": "som"
+        }
+
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+        user_exists = get_user_model().objects.filter(
+            email=payload.get("email")
+        ).exists()
+        self.assertFalse(user_exists)
